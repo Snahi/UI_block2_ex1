2569eeefa8707753a462c413d1c096948b4da5d8
Snahi <good.letterbox@gmail.com>
2019-10-10 22:45:15 +0200

    - delete activity complete

--- scripts/index_script.js
+++ scripts/index_script.js
@@ -21,6 +21,9 @@ function fitFontSizeForActivities()
 
 function fitFontSizeForActivity(text, image)
 {
+    if (!isMultiLine(text)) // adjust size only for single line texts
+        return;
+
     var textHeight  = getElementHeight(text);
     var imageHeight = getElementHeight(image);
 
@@ -33,16 +36,25 @@ function fitFontSizeForActivity(text, image)
         text.style.fontSize = fontSize + "px";
 
         textHeight = getElementHeight(text);
-
-        console.log(textHeight + " " + imageHeight);
     }
 
     // in case text is bigger than image, then come back to previous value
     if (textHeight > imageHeight)
     {
         text.style.fontSize = fontSize - 1 + "px";
-        image.style.height  = getElementHeight(text);
+        image.style.height  = getElementHeight(text);   // adjust image to text so that they are the same in height
     }
+}
+
+
+
+function isMultiLine(text)
+{
+    var textStyle       = window.getComputedStyle(text);
+    var lineHeightStr   = textStyle.lineHeight;
+    var lineHeight      = lineHeightStr.substr(0, lineHeightStr.length - 2);
+
+    return getElementHeight(text) / lineHeight > 1;
 }
 
 
@@ -62,16 +74,37 @@ function getElementHeight(element)
 
 function closeActivity(activityToClose)
 {
-    // TODO show confirmation dialog before
-    // first make activity disappear
-    activityToClose.style.opacity   = "0.0";
+    var dialog  = document.getElementById("confirm_activity_delete_popup");
+    var butYes  = document.getElementById("yes_button");
+    var butNo   = document.getElementById("no_button");
 
-    // and then fill the empty space with other activities (set total height of deleted activity to 0)
-    activityToClose.style.height    = "0px";
-    activityToClose.style.margin    = "0px";
-    activityToClose.style.padding   = "0px";
+    dialog.style.display = "flex";
 
-    setTimeout(function() {activityToClose.style.display = "none";}, 1500)
+    butYes.onclick = function()
+    {
+        dialog.style.display = "none";
+        getDeleteActivityFunction(activityToClose)();
+    };
+
+    butNo.onclick   = function() {dialog.style.display = "none";}
+}
+
+
+
+function getDeleteActivityFunction(activityToClose)
+{
+    return function()
+    {
+        // first make activity disappear
+        activityToClose.style.opacity   = "0.0";
+
+        // and then fill the empty space with other activities (set total height of deleted activity to 0)
+        activityToClose.style.height    = "0px";
+        activityToClose.style.margin    = "0px";
+        activityToClose.style.padding   = "0px";
+
+        setTimeout(function() {activityToClose.style.display = "none";}, 1500)
+    }
 }
 
 
